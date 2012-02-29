6fc2e96b10084507c4d64290dcb66beb8b04d9b2
robertabcd <robertabcd@gmail.com>
2012-02-29 22:21:44 +0800

    refine endian detection

--- endian.h
+++ endian.h
@@ -1,12 +1,12 @@
-#ifndef _ENDIAN_H
-#   define _ENDIAN_H
+#ifndef AMF_ENDIAN_H
+#   define AMF_ENDIAN_H
 
 #ifdef __BYTE_ORDER
 #   if __BYTE_ORDER == __LITTLE_ENDIAN
 #	ifndef __LITTLE_ENDIAN__
 #	    define __LITTLE_ENDIAN__
 #	endif
-#   else
+#   elif __BYTE_ORDER == __BIG_ENDIAN
 #	ifndef __BIG_ENDIAN__
 #	    define __BIG_ENDIAN__
 #	endif
@@ -17,7 +17,7 @@
 #   define NTOH64(x) __builtin_bswap64(x)
 #   define NTOH32(x) __builtin_bswap32(x)
 #   define NTOH16(x) (__builtin_bswap32((x) << 8) >> 8)
-#elif __BIG_ENDIAN__
+#elif defined __BIG_ENDIAN__
 #   define NTOH64(x) (x)
 #   define NTOH32(x) (x)
 #   define NTOH16(x) (x)
